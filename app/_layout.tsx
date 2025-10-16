4d01dbc173110c49575fc42fb6a5e74e27e18704
Madhavan Shah <madhavan.a.shah@learner.manipal.edu>
2025-10-16 22:31:05 +0530

    Add functionality for login - started work

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Stack } from "expo-router";
-
+const isLoggedIn = false;
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Protected guard={!isLoggedIn}>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={isLoggedIn}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack.Protected>
     </Stack>
   );
 }
